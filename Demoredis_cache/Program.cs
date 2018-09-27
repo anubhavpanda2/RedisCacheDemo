780b59b804ece483c67b133ca409b3a40c632eef
Anubhav Panda <anubhavpanda2@gmail.com>
2018-09-27 18:43:03 -0500

    redisCacheDemo

---
+++ Demoredis_cache/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace Demoredis_cache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer con = ConnectionMultiplexer.Connect("connectionstring");
+            IDatabase cache = con.GetDatabase();
+            //cache.StringSet("data","hello redis ccache");
+            //Console.WriteLine("data written");
+            string data = cache.StringGet("data");
+            Console.WriteLine(data);
+            Console.Read();
+        }
+    }
+}
